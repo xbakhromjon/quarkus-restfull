20d5df5bdb4b761ef976fc19887f94c27ef5e902
Bakhromjon Khasanboyev <xbakhromjon@gmail.com>
2022-09-17 09:01:48 +0500

    metrics, healthcheck

--- src/main/java/uz/example/RoomResource.java
+++ src/main/java/uz/example/RoomResource.java
@@ -1,20 +1,31 @@
 package uz.example;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import org.jboss.logging.Logger;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/room")
 public class RoomResource {
+
     @Inject
     RoomService roomService;
 
+    @Inject
+    MeterRegistry meterRegistry;
+
     @GET
     public List<Room> getRooms() {
+        Timer timer = Timer.builder("roomservice").tag("method", "getRooms").register(meterRegistry);
+        timer.record(10, TimeUnit.MICROSECONDS);
         return roomService.getRooms();
     }
 }
--- src/main/java/uz/example/RoomService.java
+++ src/main/java/uz/example/RoomService.java
@@ -1,14 +1,18 @@
 package uz.example;
 
+import org.jboss.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
 public class RoomService {
-
+    private final static Logger LOG = Logger.getLogger(RoomResource.class);
     public List<Room> getRooms() {
-        return List.of(new Room("r1", "r1", "r"),
+        List<Room> rooms = List.of(new Room("r1", "r1", "r"),
                 new Room("r2", "r2", "r"),
                 new Room("r3", "r3", "r"));
+        LOG.debugf("exiting getRooms");
+        return rooms;
     }
 }
